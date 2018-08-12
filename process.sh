44609c860a06498dfdfacf999e55e228d0179208
Yonathan Koren <yonkornilov@live.com>
2018-08-11 21:30:42 -0400

    add k2pdfopt command w/ args to script

--- process.sh
+++ process.sh
@@ -5,9 +5,16 @@ if [ "$#" -ne 4 ]; then
     exit 1
 fi
 
+PDF=$1
+TITLE=$2
+AUTHOR=$3
+OUTFILE=$4
+
 if ! which k2pdfopt > /dev/null; then
     echo "Error: k2pdfopt not found in PATH"
     exit 1
 fi
+
+k2pdfopt -col 1 -fc- -w 1072 -h 1448 -dpi 300 -idpi -2 -ui- -title $2 -author $3 -o $2 $1
 
 exit 0
